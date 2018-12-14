cfdd8ccbd67067799e88d4a182658c5956666e1b
janl1992 <jan.loeffelsender@gmail.com>
2018-12-14 11:50:43 +0100

    recursive function optimized

--- scripts/postgreSQL/src/Graphtraversierung/recursiveFunction.sql
+++ scripts/postgreSQL/src/Graphtraversierung/recursiveFunction.sql
@@ -1,19 +1,30 @@
 CREATE OR REPLACE FUNCTION recursivesearch(tInput integer[], iRecursionDepth integer) RETURNS SETOF integer AS $$
   Declare
-	intermDst_ integer[];
+	  intermDst_ integer[];
+	  iCount integer;
   BEGIN
-	iRecursionDepth = iRecursionDepth + 1;
+	--iRecursionDepth = iRecursionDepth + 1;
 	CREATE TABLE intermDst AS SELECT * FROM unnest(tInput);
 -- Does not return from function!
     return query SELECT DISTINCT(dst) FROM team22.public.relation_facebook WHERE src IN (SELECT * FROM intermDst);
 -- Does not return from function!
 	intermDst_ := ARRAY(SELECT DISTINCT(dst) FROM team22.public.relation_facebook WHERE src IN (SELECT * FROM intermDst));
+	raise notice 'timestamp: %', clock_timestamp();
+    SELECT count(*) INTO iCount FROM intermDst;
+    raise notice 'Count Table: %', iCount;
 	DROP TABLE intermDst;
 -- As recursion depth is 5
-	if iRecursionDepth < 5 THEN
-		return query SELECT * FROM recursivesearch(intermDst_, iRecursionDepth);
+	if iRecursionDepth > 0 THEN
+		return query SELECT * FROM recursivesearch(intermDst_, iRecursionDepth - 1);
 	ELSE
 		RETURN;
 	END IF;
   END;
 $$ LANGUAGE plpgsql;
+
+-- Im Vergleich zum kaskadierendem Select returned diese Funktion bei einer rekursionstiefe
+-- von 0  genauso viele Zeilen wie das kaskadierende Select(93). Bei einer
+-- rekursionstiefe von 2 returned diese Funktion 109 Zeilen, das kaskadierende SELECT jedoch nur 16 Zeilen.
+-- Der Grund dafür ist, dass sich die Funktion alle traversierten Knoten merkt:
+-- 93 (0. Rekursionsstufe) + 16 (1. Rekursionsstufe) = 109
+-- Das kaskadierende SELECT gibt jedoch nur die Zeilen der letzten Rekursionsstufe aus
