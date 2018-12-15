cd86abb53b5889d84920fc0385fbe6be24ea310d
janl1992 <jan.loeffelsender@gmail.com>
2018-12-15 17:28:50 +0100

    recursive function parametrized

--- scripts/postgreSQL/src/Graphtraversierung/recursiveFunction.sql
+++ scripts/postgreSQL/src/Graphtraversierung/recursiveFunction.sql
@@ -1,25 +1,27 @@
-CREATE OR REPLACE FUNCTION recursivesearch(tInput integer[], iRecursionDepth integer) RETURNS SETOF integer AS $$
-  Declare
-	  intermDst_ integer[];
-	  iCount integer;
-  BEGIN
-	--iRecursionDepth = iRecursionDepth + 1;
-	CREATE TABLE intermDst AS SELECT * FROM unnest(tInput);
--- Does not return from function!
-    return query SELECT DISTINCT(dst) FROM team22.public.relation_facebook WHERE src IN (SELECT * FROM intermDst);
--- Does not return from function!
-	intermDst_ := ARRAY(SELECT DISTINCT(dst) FROM team22.public.relation_facebook WHERE src IN (SELECT * FROM intermDst));
-	raise notice 'timestamp: %', clock_timestamp();
-    SELECT count(*) INTO iCount FROM intermDst;
-    raise notice 'Count Table: %', iCount;
-	DROP TABLE intermDst;
--- As recursion depth is 5
-	if iRecursionDepth > 0 THEN
-		return query SELECT * FROM recursivesearch(intermDst_, iRecursionDepth - 1);
-	ELSE
-		RETURN;
-	END IF;
-  END;
+CREATE OR REPLACE FUNCTION recursivesearch(tInput integer[], iRecursionDepth integer, sTable text) RETURNS SETOF integer AS $$
+Declare
+  intermDst_ integer[];
+  iCount integer;
+BEGIN
+  --iRecursionDepth = iRecursionDepth + 1;
+  CREATE TABLE intermDst AS SELECT * FROM unnest(tInput);
+  EXECUTE 'CREATE TABLE intermDst1 AS SELECT DISTINCT(dst) FROM ' || sTable || ' WHERE src IN (SELECT * FROM intermDst)';
+  -- Does not return from function!
+  return query SELECT * FROM intermDst1;
+  -- Does not return from function!
+  intermDst_ := ARRAY(SELECT * FROM intermDst1);
+  raise notice 'timestamp: %', clock_timestamp();
+  SELECT count(*) INTO iCount FROM intermDst;
+  raise notice 'Count Table: %', iCount;
+  DROP TABLE intermDst;
+  DROP TABLE intermDst1;
+  -- As recursion depth is 5
+  if iRecursionDepth > 1 THEN
+    return query SELECT * FROM recursivesearch(intermDst_, iRecursionDepth - 1);
+  ELSE
+    RETURN;
+  END IF;
+END;
 $$ LANGUAGE plpgsql;
 
 -- Im Vergleich zum kaskadierendem Select returned diese Funktion bei einer rekursionstiefe
